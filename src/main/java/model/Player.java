008ce0a08bbc669d855a20885c8643f6ce785152
KOUZOUD <kouzoudmohamed@gmail.com>
2025-04-19 16:08:59 +0200

    first version of projet

---
+++ src/main/java/model/Player.java
@@ -0,0 +1,37 @@
+package model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Player {
+    @Id 
+    private Long id;
+    private String name;
+    private Long score;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getScore() {
+        return score;
+    }
+
+    public void setScore(Long score) {
+        this.score = score;
+    }
+}
